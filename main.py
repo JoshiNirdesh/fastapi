ac0ccf09ee2221881d8de08651bcf1a26063eaf2
JoshiNirdesh <joshinirdesh10@gmail.com>
2026-08-11 10:54:57 +0545

    get_all_products

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from models import Product
 
 app = FastAPI()
 
@@ -6,3 +7,12 @@ app = FastAPI()
 def greet ():
     return("Welcome to the Server")
 
+product = [
+    Product(id=1,name="Apple",desc="Phone",price=12,quantity=2),
+    Product(id=2,name="Samsung",desc="SmartPhone",price=124,quantity=5)
+
+
+]
+@app.get("/products")
+def get_all_products ():
+    return product
